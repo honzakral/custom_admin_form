05c4b7c9bb74714a34971e73dd9565a2ff2a889a
Honza Kral <honza.kral@gmail.com>
2010-08-25 16:51:52 -0700

    Model definitions

--- adhack/adapp/models.py
+++ adhack/adapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    name = models.CharField(unique=True, max_length=231)
+    question = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+class PollOption(models.Model):
+    poll = models.ForeignKey(Poll)
+    text = models.TextField()
+    votes = models.PositiveIntegerField(editable=False, default=0)
+
+    def __unicode__(self):
+        return '%s: %s' % (self.poll, self.text)
+
+
